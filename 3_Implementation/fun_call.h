bfc5f1e853975f0a68da953eb22d4699bcdf894b
Rajasekhar <bammidirajasekhar.2018.eee@rajalakshmi.edu.in>
2022-03-30 17:36:31 +0530

    Update Employee_salary.c

---
+++ 3_Implementation/fun_call.h
@@ -0,0 +1,10 @@
+
+int add_Details(char *name_of_Employee, int no_Of_Days_Worked,int no_Of_Employees, long long int mob_No, double basic_Salary_Emp, double no_Of_hours_OT_Worked, double allowance);
+double basic_Salary_Calc(int no_Of_Days_Worked,double basic_Salary_Emp);
+double OT_Calc(double no_Of_hours_OT_Worked, double basic_Salary_Emp);
+double pf_Calc(double basic_Salary_Emp);
+void login();
+void reg();
+void display();
+void data();
+void call();
